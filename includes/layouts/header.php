d4845fbe682fe9ae17883327450e91ebc6f7e8cb
masawang116 <masawang116@gmail.com>
2014-12-07 21:33:16 -0800

    not even v1

---
+++ includes/layouts/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>FirstPHP</title>
+	</head>
+	<body>
+		<header>
+			<p>'This is the header.'</p>
+		</header>
---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+
+<?php 
+require_once('../includes/connect_to_db.php');
+include '../includes/layout/header.php'; 
+?> 
+<?php
+connect_to_db();
+?>
+<?php
+include '../includes/layout/footer.php';
+?>
